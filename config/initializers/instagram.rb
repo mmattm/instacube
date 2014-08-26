155cc755840adc0ffc0e48376dbd4bf10fe0f506
Matthieu Minguet <minguet.mat@gmail.com>
2014-08-26 15:24:57 +0200

    heroku

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,5 +1,5 @@
 require "instagram"
 Instagram.configure do |config|
 config.client_id = '1515381849524d6c8b87d4e4b917835f'
-config.access_token = SUPER_SECRET_TOKEN
+config.access_token = "351244118.1515381.eebfac413392427ca88ce9fdf65db00e"
 end
