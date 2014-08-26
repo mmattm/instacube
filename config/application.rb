82f08449c0243ab893b72e179ebca8ff37a2d636
Matthieu Minguet <minguet.mat@gmail.com>
2014-08-26 16:14:10 +0200

    a

--- config/application.rb
+++ config/application.rb
@@ -7,12 +7,14 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Instamongo
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
