50e0b81e50b7f9917b4a90b6080a4dd02ab0c50c
Matthieu Minguet <minguet.mat@gmail.com>
2014-08-27 12:11:07 +0200

    test

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require File.expand_path('../boot', __FILE__)
 # require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
-#require "active_resource/railtie" # Comment this line for Rails 4.0+
+require "active_resource/railtie" # Comment this line for Rails 4.0+
 require "rails/test_unit/railtie"
 # require "sprockets/railtie" # Uncomment this line for Rails 3.1+
 
